7883f04bfeaeed0031b8a94e6e0b2362bdbd20b7
yohei-kamon <yoyofreestyle0@gmail.com>
2026-01-12 15:33:07 +0900

    課題完了

--- a-10.py
+++ a-10.py
@@ -1,11 +1,12 @@
 import random
 
 
-def dice(list):
-    result = random.choice(list)
+def dice():
+    result = random.randint(1, 6)
     return result
 
 
-list = [1, 2, 3, 4, 5, 6]
+print(dice())
+# list = [1, 2, 3, 4, 5, 6]
 
-print(dice(list))  # 1から6の整数をランダムに出力する
+# print(dice(list))  # 1から6の整数をランダムに出力する
